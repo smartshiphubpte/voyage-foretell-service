2cb8f46b5f4b007be5d57ec6593d3f9ad3f661e5
magar amol <106598063+magaramol01@users.noreply.github.com>
2024-02-08 14:46:06 +0530

    refactor predict noon

--- app/database_.py
+++ app/database_.py
@@ -1,23 +1,48 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+import asyncpg  # Ensure asyncpg is imported
 
-# Load environment variables from .env file
-load_dotenv()
+# TODO: load from the dotenv
 
-# Database configuration from environment variables
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:" \
-               f"{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:" \
-               f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DB_USER= "postgres"
+DB_PASSWORD= "postgres"
+DB_HOST= "localhost"
+DB_PORT= 5432
+DB_NAME= "postgres"
+DB_SCHEMA= "shipping_db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+# DB_USER = "postgres"
+# DB_PASSWORD = "bzeUm@)=J=*5XRbI"
+# DB_HOST = "34.93.52.26"
+# DB_PORT = 5432
+# DB_NAME = "uat"
+# DB_SCHEMA = "shipping_db"
+
+# Construct the DATABASE_URL using the variables
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print(f"DATABASE_URL {DATABASE_URL}")
+
+# Previous SQLAlchemy engine setup is commented out to keep for reference
+# engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Configure the Session class to be used for creating sessions
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+# AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+# New asyncpg connection pool setup
+async def create_db_pool():
+    return await asyncpg.create_pool(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
 
 async def get_db():
-    async with AsyncSessionLocal() as session:
-        await session.execute(f"SET search_path TO {os.getenv('DB_SCHEMA')}")
-        await session.commit()
-        yield session
+    pool = await create_db_pool()
+    async with pool.acquire() as connection:
+        # Optionally, set the schema for the session
+        # await connection.execute(f"SET search_path TO {DB_SCHEMA}")
+        yield connection
+        # The pool and connection are automatically released/closed when exiting the async context
--- app/routers/route_fule_optimization_for_good_weather_.py
+++ app/routers/route_fule_optimization_for_good_weather_.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends ,HTTPException
 from app.database_ import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import text
 import numpy as np
 import pandas as pd
 import joblib
+import xmltodict 
 from app.validator.validator_fule_optimization_for_good_weather_ import Input
 from scipy.optimize import differential_evolution
 from datetime import datetime, timedelta
 from app.utils.utils_route_fule_optimization_for_good_weather_ import to_model_required_format ,fuel_consumption_objective
-
+from app.controller.controller_fule_optimization_for_good_weather_ import predict_fuel_consumption
 router = APIRouter()
 
 # Load the trained model
@@ -18,50 +19,50 @@ feature_transformer = joblib.load('app/models/orion/feature_transformer.joblib')
 target_transformer = joblib.load('app/models/orion/target_transformer.joblib')
 
 
-@router.get("/predict")
-async def predict_fuel_consumption(db: AsyncSession = Depends(get_db)):
-    return  (await db.execute(text("SELECT * FROM shipping_db.user"))).fetchall()
+@router.get("/predict_noon/{vesselid}")
+async def predict_fuel_consumptions_from_last_noon(vesselid: int, db=Depends(get_db)):
+    query = """
+        SELECT
+        noonreportdata ->> 'Slip' AS "slip",
+        noonreportdata ->> 'ME_RPM' AS "me_rpm",
+        noonreportdata ->> 'Distance' AS "distance",
+        noonreportdata ->> 'Avg_Speed' AS "avg_speed",
+        noonreportdata ->> 'Draft_AFT' AS "draft_aft",
+        noonreportdata ->> 'Draft_FWD' AS "draft_fwd",
+        noonreportdata ->> 'Sea_Height' AS "sea_height",
+        noonreportdata ->> 'Wind_Speed' AS "wind_speed",
+        noonreportdata ->> 'Swell_Height' AS "swell_height",
+        noonreportdata ->> 'Distance_Remaining_To_EOV' AS "distance_remaining_to_eov",
+        noonreportdata ->> 'ME_Running_Hrs' AS "me_running_hrs",
+        noonreportdata ->> 'Douglas_Sea_State' AS "douglas_sea_state",
+        noonreportdata ->> 'Average_Speed_Since_SOV' AS "average_speed_since_sov",
+        noonreportdata ->> 'Distance_Covered_Since_SOV' AS "distance_covered_since_sov",
+        noonreportdata ->> 'Total_HFO_Consumed_In_MT' AS "total_hfo_consumed_in_mt",
+        noonreportdata ->> 'ETA_Next_Port' AS "eta_next_port",
+        noonreportdata ->> 'Total_HFOME_Consumed_In_MT' AS "total_hfome_consumed_in_mt",
+        noonreportdata ->> 'Total_ULSGOME_Consumed_In_MT' AS "total_ulsgome_consumed_in_mt",
+        noonreportdata ->> 'Total_VLSFOME_Consumed_In_MT' AS "total_vlsfome_consumed_in_mt",
+        noonreportdata ->> 'Total_VLSGOME_Consumed_In_MT' AS "total_vlsgome_consumed_in_mt"
+    FROM (
+        SELECT *,
+            row_number() OVER (ORDER BY id DESC) as rn
+        FROM shipping_db.enoonreporttable
+        WHERE vesselid = $1
+    ) AS subquery
+    WHERE rn = 2;
+    """
+    result = await db.fetch(query, vesselid)
+    if result:
+        # Convert the asyncpg.Record to a standard Python dictionary
+        record_dict = dict(result[0])
+        
+        prediction = await predict_fuel_consumption(record_dict)
+        
+        return prediction
+    else:
+        return {"error": "No data found for vessel ID {}".format(vesselid)}
 
 @router.post("/predict_noon")
 async def predict_noon(item: Input):
-    # adding lag values
-    item_dict = to_model_required_format(item)
-    
-    converted_data = np.array(list(item_dict.values())).reshape(1, -1)
-    # Example usage:
-    remaining_distance = item_dict["distance_remaining_to_eov"]  # Replace with actual remaining distance
-    days_remaining = item_dict["days_remaining"]
-    speed_bounds = (8, 20)
-
-# Perform differential evolution optimization
-    result = differential_evolution(
-    fuel_consumption_objective,
-    bounds=[speed_bounds],
-    args=(model, target_transformer ,converted_data),
-    maxiter=100,
-    popsize=10,
-    disp=True
-    )
-    result
-    # Extract results
-    optimized_speed = result.x[0]
-    optimized_fuel_consumption = result.fun
-
-    # Calculate total fuel consumption for the remaining voyage
-    total_fuel_consumption = optimized_fuel_consumption * remaining_distance
-
-    # Calculate fuel consumption per day for the remaining days
-    fuel_consumption_per_day = total_fuel_consumption / days_remaining if days_remaining != 0 else 0
-
-    # Create a DataFrame for future dates
-    today_at_noon = datetime.now().replace(hour=12, minute=0, second=0, microsecond=0)
-
-    future_dates = [today_at_noon + timedelta(days=i) for i in range(1, days_remaining + 1)]
-
-    # Create a DataFrame with dates, optimized speed, and fuel consumption per day
-    return  pd.DataFrame({
-        'Date':  future_dates,
-        'Optimized Speed': [optimized_speed] * days_remaining,
-        'Fuel Consumption per Day': [fuel_consumption_per_day] * days_remaining
-    }).to_dict('records')
+    return await predict_fuel_consumption(item)
     
